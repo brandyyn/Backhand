824834a8fadc3e3a23dfddcfe8c8c3b053910b7e
brandyn <sonicbrandyn@gmail.com>
2026-05-05 01:52:09 +0100

    Update MixinModelBiped.java

--- src/main/java/xonin/backhand/mixins/early/minecraft/MixinModelBiped.java
+++ src/main/java/xonin/backhand/mixins/early/minecraft/MixinModelBiped.java
@@ -36,6 +36,7 @@ public abstract class MixinModelBiped extends ModelBase {
         at = @At(value = "FIELD", target = "Lnet/minecraft/client/model/ModelBiped;isSneak:Z", shift = At.Shift.BEFORE))
     private void backhand$moveOffHandArm(float f1, float f2, float f3, float f4, float f5, float f6, Entity entity,
         CallbackInfo ci) {
+        if (entity == null) return;
         BackhandRenderHelper.moveOffHandArm(entity, (ModelBiped) (Object) this, f3 - entity.ticksExisted);
     }
 
